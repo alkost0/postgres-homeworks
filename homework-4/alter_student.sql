e920405dfd414a12b1bb6e2ffc7ca4bed759abbc
AlKost <134562029+alkost0@users.noreply.github.com>
2023-08-10 16:39:24 +0400

    Update alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,36 @@
--- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+2-- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+CREATE TABLE student
+(
+	student_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
 
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
 
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
 
+ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
 
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
 
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Ivan', 'Ivanov', '1999-11-11', '8-846-990-90-90');
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Petr', 'Petrov', '1988-10-10', '8-846-980-80-80');
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('Nikolay', 'Sidorov', '1977-09-09', '8-846-970-70-70');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+
+TRUNCATE TABLE student RESTART IDENTITY;
